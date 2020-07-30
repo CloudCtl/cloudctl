e1e373d219a8f57e793c566b874890cb08d277c0
usrbinkat <kat@braincraft.io>
2020-07-30 10:59:19 -0600

    add devel artifacts

--- entrypoint/build.sh
+++ entrypoint/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
-goCmd=(which go)
-goCmd="/usr/local/go/bin/go"
+goCmd=$(which go)
+#goCmd="/usr/local/go/bin/go"
+${goCmd} get github.com/go-git/go-git
 ${goCmd} build koffer.go
 cp -f koffer /bin/koffer
