447974e635b9c627a2b10107e8c4f6feba780329
usrbinkat <usrbinkat@braincraft.io>
2020-08-01 00:22:16 -0600

    update entrypoint

--- entrypoint/koffer.go
+++ entrypoint/koffer.go
@@ -9,10 +9,16 @@ import (
     "sync"
     "os/exec"
     "strings"
+    "io/ioutil"
     "github.com/go-git/go-git"
     "github.com/go-git/go-git/plumbing"
-    "github.com/codesparta/koffer/entrypoint"
+//  "github.com/codesparta/koffer/entrypoint/src"
 )
+
+var pullsecret []byte
+var secretpath = "/root/.docker/"
+var secretfile = "config.json"
+var secretfilepath = (secretpath + secretfile)
 
 // Basic example of how to clone a repository using clone options.
 func main() {
@@ -24,6 +30,9 @@ func main() {
     pathClone := flag.String("dir", "/root/koffer", "Clone Path")
 
     flag.Parse()
+
+    promptQuaySecretReq()
+    createSecretFile()
 
     // build url from vars
     gitslice := []string{ "https://", *svcGit, "/", *orgGit, "/", *repoGit }
@@ -164,4 +173,38 @@ func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
 			return out, err
 		}
 	}
+}
+
+func promptQuaySecretReq() {
+  fmt.Print(`
+  Please input your Quay.io Openshift Pull Secret.
+  Find your secret at this url with valid access.redhat.com login:
+
+    https://cloud.redhat.com/openshift/install/metal/user-provisioned
+
+  Paste Secret:  `)
+  fmt.Scanln(&pullsecret)
+}
+
+func createSecretFile() {
+    if _, err := os.Stat(secretpath); os.IsNotExist(err) {
+        os.Mkdir(secretpath, os.FileMode(0600))
+    }
+    var _, err = os.Stat(secretfilepath)
+    if os.IsNotExist(err) {
+        file, err := os.Create(secretfilepath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+        defer file.Close()
+    }
+
+    err = ioutil.WriteFile("/root/.docker/config.json", pullsecret, 0600)
+    if err != nil {
+        fmt.Println(err)
+	return
+    }
+
+    fmt.Println("Created: /root/.docker/config.json")
 }
