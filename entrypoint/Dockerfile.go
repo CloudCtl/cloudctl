27ac02c257ae3906e511e3334b47294e94bd1c15
usrbinkat <usrbinkat@braincraft.io>
2020-08-03 00:58:13 -0600

    remove local koffer binary development

--- entrypoint/Dockerfile.go
+++
@@ -1,5 +0,0 @@
-FROM docker.io/ocpredshift/red-gotools
-WORKDIR /root/dev
-RUN set -ex \
-     && ./build.sh \
-    && echo
--- entrypoint/podman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo podman run \
-    -it --rm --net=host \
-    --volume $(pwd):/root/dev \
-    --volume ~/.ssh:/root/.ssh \
-    --volume ~/.bashrc:/root/.bashrc \
-    --volume ~/.gitconfig:/root/.gitconfig \
-    --name entrypoint --hostname entrypoint \
-  docker.io/ocpredshift/red-gotools -c /usr/bin/tmux
