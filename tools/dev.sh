c8cb9a6de6115d298f364e31eaff3d6b85ff5c89
usrbinkat <usrbinkat@braincraft.io>
2020-09-24 20:04:06 +0000

    tweak

--- tools/dev.sh
+++ tools/dev.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -x
-clear
+project="cloudctl"
+
+# Prep SSH, Credentials, & Bash
 rm -rf /tmp/.ssh /tmp/.gitconfig /tmp/.bashrc
 cp -rf ~/.ssh ~/.gitconfig ~/.bashrc /tmp/
 sudo podman run -it --rm --pull always \
-    -h konductor --name konductor \
+    -h ${project} --name ${project} \
     --entrypoint bash --privileged \
     --volume /tmp/.ssh:/root/.ssh:z \
     --volume /tmp/.bashrc:/root/.bashrc:z \
     --volume /tmp/.gitconfig:/root/.gitconfig:z \
-    --volume $(pwd):/root/platform/iac/cloudctl:z \
-    --workdir /root/platform/iac/cloudctl \
+    --volume $(pwd):/root/platform/iac/${project}:z \
+    --workdir /root/platform/iac/${project} \
   docker.io/codesparta/konductor:latest
