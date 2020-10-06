788079a63032978b4032fa37688ee89086987628
usrbinkat <usrbinkat@braincraft.io>
2020-10-06 12:43:20 -0700

    flip collector dev to koffer img

--- tools/collector-dev.sh
+++ tools/collector-dev.sh
@@ -12,4 +12,4 @@ sudo podman run -it --rm --pull always \
     --volume /tmp/.gitconfig:/root/.gitconfig:z \
     --volume $(pwd):/root/koffer:z \
     --workdir /root/koffer/ \
-  docker.io/containercraft/konductor:latest
+  docker.io/containercraft/koffer:latest
