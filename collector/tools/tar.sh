d47b877e14b17667cfb14b3dbed824d57824603b
usrbinkat <usrbinkat@braincraft.io>
2020-10-26 13:00:23 -0700

    polish

--- collector/tools/tar.sh
+++ collector/tools/tar.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 # Offline Image Table
 declare -A img_table=(\
@@ -25,7 +25,7 @@ for img in "${!img_table[@]}"; do
 
   # Pull IMG
   echo ">> Pulling ${img} Image" 
-  podman pull "${img_table[$img]}":latest
+  podman pull -s "${img_table[$img]}":latest
 
   # Save IMG
   echo ">> Saving ${img} Image" 
