631d8e4b92df742bc41eb9641f6ebce7b384b185
usrbinkat <usrbinkat@braincraft.io>
2020-10-26 11:04:13 -0700

    update wip

--- konductor.sh
+++ konductor.sh
@@ -1,24 +1,28 @@
 #!/bin/bash -x
 project="konductor-init"
-
-# Check if offline konductor image available for import
 OFFLINE_IMG="$(pwd)/bundle/image-konductor-latest.tar"
-if [[ -f ${OFFLINE_IMG} ]]; then
-    echo ">> Loading Konductor Image from ${OFFLINE_IMAGE}"
-    podman load --input $(pwd)/bundle/image-konductor-latest.tar
-    podman tag docker.io/containercraft/konductor:latest docker.io/containercraft/konductor:init
-    podman images --format "{{.ID}}"
-else
-    echo ">> Loading Konductor Image from DockerHub Repo"
-    podman pull docker.io/containercraft/konductor:latest
-    podman tag docker.io/containercraft/konductor:latest \
-               docker.io/containercraft/konductor:init
-fi
 
 # Stage SSH Credentials
 sudo rm -rf /tmp/.ssh
 sudo cp -rf ~/.ssh /tmp/
-sudo podman run -it --rm \
+
+# Load konductor image
+if [[ -f ${OFFLINE_IMG} ]]; then
+    # Pull image from local tar file
+    echo ">> Loading Konductor Image from ${OFFLINE_IMAGE}"
+    podman load --input $(pwd)/bundle/image-konductor-latest.tar
+else
+    # Pull from public internet if tar not found 
+    echo ">> Loading Konductor Image from DockerHub Repo"
+    podman pull docker.io/containercraft/konductor:latest
+fi
+
+# Detect and tag image to non-latest tag for offline support
+IMG_SHA=$(podman images --format "{{.ID}} {{.Names}}" --no-trunc | awk -F'[ :]' '/konductor:latest/{print $2}' | head -n1)
+podman tag ${IMG_SHA} docker.io/containercraft/konductor:init
+
+# Run konductor image
+time sudo podman run -it --rm \
     -h ${project} --name ${project}               \
     --entrypoint ./site.yml --privileged          \
     --volume /tmp/.ssh:/root/.ssh:z               \
