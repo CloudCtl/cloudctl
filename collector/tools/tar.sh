6a9333c5362b86c651e6e7d3ef4414b951fd1380
usrbinkat <usrbinkat@braincraft.io>
2020-10-26 12:29:50 -0700

    tagging fix

--- collector/tools/tar.sh
+++ collector/tools/tar.sh
@@ -1,3 +1,19 @@
-podman pull docker.io/containercraft/konductor:latest
-podman save --output bundle/image-konductor-latest.tar --format oci-archive docker.io/containercraft/konductor:latest
+#!/bin/bash -ex
+declare -A img_table=(\
+  ['konductor']="docker.io/containercraft/konductor" \
+  ['runner']="docker.io/containercraft/ansible-runner-service" \
+  ['registry']="docker.io/library/registry" \
+  ['pause']="k8s.gcr.io/pause" \
+)
+
+for img in "${!img_table[@]}"; do
+  echo ">> Pulling ${img} Image" 
+  podman pull "${img_table[$img]}":latest
+  echo ">> Saving ${img} Image" 
+  podman save \
+   --output bundle/image-${img}-latest.tar \
+   --format oci-archive "${img_table[$img]}":latest
+  echo ">> File Saved: $(du -sh ./bundle/image-${img}-latest.tar)" 
+done
+
 podman image prune --all --force
--- init.sh
+++ init.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
-project="konductor-init"
-OFFLINE_IMG="$(pwd)/bundle/image-konductor-latest.tar"
+project="init"
+OFFLINE_KONDUCTOR="$(pwd)/bundle/image-konductor-latest.tar"
+OFFLINE_REGISTRY="$(pwd)/bundle/image-registry-latest.tar"
+OFFLINE_RUNNER="$(pwd)/bundle/image-runner-latest.tar"
+OFFLINE_PAUSE="$(pwd)/bundle/image-pause-latest.tar"
 
 # Stage SSH Credentials
 sudo rm -rf /tmp/.ssh
 sudo cp -rf ~/.ssh /tmp/
 
 # Load konductor image
-if [[ -f ${OFFLINE_IMG} ]]; then
-    # Pull image from local tar file
-    echo ">> Loading Konductor Image from ${OFFLINE_IMG}"
-    podman load --input $(pwd)/bundle/image-konductor-latest.tar
+if [[ -f ${OFFLINE_KONDUCTOR} ]]; then
+
+  # Clean/Prep
+  podman pod rm --force cloudctl
+  podman image prune --all --force
+
+  # Pull images from local tar files
+  for IMG in \
+    ${OFFLINE_PAUSE} \
+    ${OFFLINE_RUNNER} \
+    ${OFFLINE_REGISTRY} \
+    ${OFFLINE_KONDUCTOR} \
+  ; do
+      echo ">> Loading Konductor Image from ${IMG}"
+      podman load --input ${IMG}
+  done
 else
+
     # Pull from public internet if tar not found 
-    echo ">> Loading Konductor Image from DockerHub Repo"
+    echo ">> Pulling Konductor Image from DockerHub Repo"
     podman pull docker.io/containercraft/konductor:latest
 fi
 
-# Detect and tag image to non-latest tag for offline support
-IMG_SHA=$(podman images --format "{{.ID}} {{.Names}}" --no-trunc | awk -F'[ :]' '/konductor:latest/{print $2}' | head -n1)
-podman tag ${IMG_SHA} docker.io/containercraft/konductor:init
-
 # Run konductor image
-time sudo podman run -it --rm \
+time sudo podman run -it --rm --pull never \
     -h ${project} --name ${project}               \
     --entrypoint ./site.yml --privileged          \
     --volume /tmp/.ssh:/root/.ssh:z               \
     --workdir /root/platform/iac/cloudctl         \
     --volume $(pwd):/root/platform/iac/cloudctl:z \
-  docker.io/containercraft/konductor:init
+  docker.io/containercraft/konductor:latest
