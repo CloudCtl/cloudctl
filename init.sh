adbffb580a11a7f5821fb042d7d8443d3eedbf51
usrbinkat <usrbinkat@braincraft.io>
2020-11-18 23:17:15 -0800

    redact init.sh

--- init.sh
+++ init.sh
@@ -5,37 +5,29 @@ IMPORT_LIST="$(ls ${dir_seed})"
 dir_bundle="$(pwd)/bundle"
 EXTRACT_LIST="$(ls ${dir_bundle}/*.tar.xz 2>/dev/null)"
 
-# Extract bundles
+sudo rm -rf /tmp/.ssh
+sudo cp -rf ~/.ssh /tmp/
+
 if [[ ! -z "${IMPORT_LIST}" ]]; then
   for i in ${EXTRACT_LIST}; do
     tar xvf ${i} -C /root
   done
 fi
 
-# Stage SSH Credentials
-sudo rm -rf /tmp/.ssh
-sudo cp -rf ~/.ssh /tmp/
-
-# Load cloudctl seed images
 if [[ ! -z "${IMPORT_LIST}" ]]; then
 
-  # Clean/Prep
   podman pod rm --force cloudctl 2>/dev/null
   podman image prune --all --force
 
-  # Pull images from local tar files
   for IMG in ${IMPORT_LIST}; do
       echo ">> Loading Konductor Image from ${IMG}"
       podman load --input ${dir_seed}/${IMG}
   done
 else
-
-    # Pull from public internet if tar not found 
     echo ">> Pulling Konductor Image from DockerHub Repo"
     podman pull quay.io/cloudctl/konductor:latest
 fi
 
-# Run konductor image
 time sudo podman run -it --rm --pull never \
     -h ${project} --name ${project}               \
     --entrypoint ./site.yml --privileged          \
